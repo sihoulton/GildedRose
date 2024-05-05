86b02b4ea81e2ddf93034f13c608b7e40b0f1251
Simon Houlton <si.houlton@spitfiresoftwarehouse.com>
2024-05-05 13:40:58 +0100

    Introduce Dexterity Vest Tests, should have introduced these before amending main code.

---
+++ GildedRoseTests/DexterityVestTests.cs
@@ -0,0 +1,33 @@
+﻿using GildedRoseKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class DexterityVestTests
+    {
+        [Fact]
+        public void ShouldDegradeQualityBeforeExpiry()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 1, Quality = 1 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(0, Items[0].Quality);
+        }
+
+        [Fact]
+        public void ShouldDegradeQualityBy2AfterExpiry()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 2 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(0, Items[0].Quality);
+        }
+    }
+}
