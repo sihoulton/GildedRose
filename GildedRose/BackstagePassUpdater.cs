6c9af0cc377858b2d29b2e43b30ad623b7465e1e
Simon Houlton <si.houlton@spitfiresoftwarehouse.com>
2024-05-05 13:34:37 +0100

    Backstage Pass updater tests written, code yet to be migrated.

---
+++ GildedRose/BackstagePassUpdater.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata
+{
+    public class BackstagePassUpdater : IInventoryUpdater
+    {
+        private Item _item;
+
+        public Item UpdateItem(Item item)
+        {
+            _item = item;
+
+            UpdateQuality();
+            UpdateSellIn();
+
+            return _item;
+        }
+
+        private void UpdateQuality()
+        {
+            IncrementQuality();
+
+            ProtectQualityParametersOrReset();
+        }
+
+        private void IncrementQuality()
+        {
+            _item.Quality++;
+
+            if (_item.SellIn < 11)
+            {
+                _item.Quality++;
+            }
+
+            if (_item.SellIn < 6)
+            {
+                _item.Quality++;
+            }
+
+        }
+
+        private void ProtectQualityParametersOrReset()
+        {
+            if (_item.Quality > 50)
+                _item.Quality = 50;
+
+            if (_item.SellIn < 1)
+                _item.Quality = 0;
+        }
+
+        private void UpdateSellIn()
+        {
+            _item.SellIn--;
+        }
+    }
+}
--- GildedRoseTests/BackStagePassTests.cs
+++ GildedRoseTests/BackStagePassTests.cs
@@ -43,5 +43,41 @@ namespace GildedRoseTests
 
             Assert.Equal(0, Items[0].Quality);
         }
+
+        [Fact]
+        public void UpdaterShouldIncreaseInQualityBy2Between10And5Days()
+        {
+            var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 35 };
+            var updater = new BackstagePassUpdater();
+
+            item = updater.UpdateItem(item);
+
+            Assert.Equal(4, item.SellIn);
+            Assert.Equal(38, item.Quality);
+        }
+
+        [Fact]
+        public void UpdaterShouldIncreaseInQualityBy3Between5And0Days()
+        {
+            var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 1 };
+            var updater = new BackstagePassUpdater();
+
+            item = updater.UpdateItem(item);
+
+            Assert.Equal(4, item.SellIn);
+            Assert.Equal(4, item.Quality);
+        }
+
+        [Fact]
+        public void UpdaterShouldSetQualityToZeroAfterEvent()
+        {
+            var item = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 10 };
+            var updater = new BackstagePassUpdater();
+
+            item = updater.UpdateItem(item);
+
+            Assert.Equal(-1, item.SellIn);
+            Assert.Equal(0, item.Quality);
+        }
     }
 }
