c847650843cfd80ecdd26df30b5a91dabfbae350
Simon Houlton <si.houlton@spitfiresoftwarehouse.com>
2024-05-05 16:10:36 +0100

    Conjured refactored

---
+++ GildedRose/Implementations/Updaters/Conjured/ConjuredQaulityUpdater.cs
@@ -0,0 +1,16 @@
+﻿namespace GildedRoseKata
+{
+    public class ConjuredQaulityUpdater : IQualityUpdater
+    {
+        public void UpdateQuality(Item item)
+        {
+            item.Quality--;
+
+            if (item.Quality <= 2)
+                item.Quality--;
+
+            if (item.Quality < 0)
+                item.Quality = 0;
+        }
+    }
+}
---
+++ GildedRose/Implementations/Updaters/Conjured/ConjuredUpdater.cs
@@ -0,0 +1,26 @@
+﻿namespace GildedRoseKata
+{
+    public class ConjuredUpdater : IInventoryUpdater
+    {
+        private IQualityUpdater _conjuredUpdater;
+        private ISellInUpdater _sellInUpdater;
+
+        public ConjuredUpdater()
+        {
+            _conjuredUpdater = new ConjuredQaulityUpdater();
+            _sellInUpdater = new StandardSellInUpdater();
+        }
+
+        public static string ItemNameToProcess() => "Conjured *";
+
+        public Item UpdateItem(Item item)
+        {
+            _conjuredUpdater.UpdateQuality(item);
+            _sellInUpdater.UpdateSellIn(item);
+
+            return item;
+        }
+
+
+    }
+}
