9764147b371f245c72d5d6cc790b3d7608a1b7a4
Simon Houlton <si.houlton@spitfiresoftwarehouse.com>
2024-05-05 12:52:50 +0100

    Aged Brie Max 50 Test

--- GildedRoseTests/AgedBrieTests.cs
+++ GildedRoseTests/AgedBrieTests.cs
@@ -20,5 +20,15 @@ namespace GildedRoseTests
 
             Assert.Equal(1, Items[0].Quality);
         }
+
+        [Fact]
+        public void ShouldMaxQualityAt50()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 30, Quality = 50 } };
+            GildedRoseKata.GildedRose app = new GildedRoseKata.GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(50, Items[0].Quality);
+        }
     }
 }
