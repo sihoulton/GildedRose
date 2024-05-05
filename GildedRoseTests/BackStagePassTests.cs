d13f07e279af3db97c09cec8107d1f85b6c9d63b
Simon Houlton <si.houlton@spitfiresoftwarehouse.com>
2024-05-05 12:57:16 +0100

    Introduce Backstage Passes

---
+++ GildedRoseTests/BackStagePassTests.cs
@@ -0,0 +1,23 @@
+﻿using GildedRoseKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class BackStagePassTests
+    {
+        [Fact]
+        public void ShouldIncreaseInQualityAfterDayWhenEventOver10DaysAway()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 1 } };
+            GildedRoseKata.GildedRose app = new GildedRoseKata.GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(2, Items[0].Quality);
+        }
+    }
+}
