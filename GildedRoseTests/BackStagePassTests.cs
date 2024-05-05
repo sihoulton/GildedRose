c1ce9d0853fb96b37b1c696e21b8a0eed6aa08b8
Simon Houlton <si.houlton@spitfiresoftwarehouse.com>
2024-05-05 13:01:57 +0100

    Backstage Pass Tests for differing rules

--- GildedRoseTests/BackStagePassTests.cs
+++ GildedRoseTests/BackStagePassTests.cs
@@ -5,19 +5,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace GildedRoseTests
 {
     public class BackStagePassTests
     {
         [Fact]
-        public void ShouldIncreaseInQualityAfterDayWhenEventOver10DaysAway()
+        public void ShouldIncreaseInQualityBy2Between10And5Days()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 1 } };
-            GildedRoseKata.GildedRose app = new GildedRoseKata.GildedRose(Items);
+            GildedRose app = new GildedRose(Items);
+
             app.UpdateQuality();
 
             Assert.Equal(2, Items[0].Quality);
+        }
+
+        [Fact]
+        public void ShouldIncreaseInQualityBy3Between5And0Days()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = 1 } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.Equal(4, Items[0].Quality);
+        }
+
+        [Fact]
+        public void ShouldSetQualityToZeroAfterEvent()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.Equal(0, Items[0].Quality);
         }
     }
 }
