f6334735c9308b814f61da15a9b7ab4b6e2154e1
Simon Houlton <si.houlton@spitfiresoftwarehouse.com>
2024-05-05 12:51:19 +0100

    Start to break out tests - Aged Brie

---
+++ GildedRoseTests/AgedBrieTests.cs
@@ -0,0 +1,24 @@
+﻿using GildedRoseKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class AgedBrieTests
+    {
+        [Fact]
+        public void ShouldIncreaseInQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1, Quality = 0 } };
+            GildedRoseKata.GildedRose app = new GildedRoseKata.GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Assert.Equal(1, Items[0].Quality);
+        }
+    }
+}
