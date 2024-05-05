9fce25f760718a2d9714df828736263bc58a8624
Simon Houlton <si.houlton@spitfiresoftwarehouse.com>
2024-05-05 16:13:55 +0100

    dexterity vest refactored

---
+++ GildedRose/Implementations/Updaters/DexterityVest/DexterityVestQualityUpdater.cs
@@ -0,0 +1,16 @@
+﻿namespace GildedRoseKata
+{
+    public class DexterityVestQualityUpdater : IQualityUpdater
+    {
+        public void UpdateQuality(Item item)
+        {
+            if (item.Quality > 10)
+                item.Quality--;
+            else if (item.Quality <= 10)
+                item.Quality = item.Quality - 2;
+
+            if (item.Quality < 0)
+                item.Quality = 0;
+        }
+    }
+}
---
+++ GildedRose/Implementations/Updaters/DexterityVest/DexterityVestUpdater.cs
@@ -0,0 +1,25 @@
+﻿namespace GildedRoseKata
+{
+    public class DexterityVestUpdater : IInventoryUpdater
+    {
+        private IQualityUpdater _dexterityVestQualityUpdater;
+        private ISellInUpdater _sellInUpdater;
+
+        public DexterityVestUpdater()
+        {
+            _dexterityVestQualityUpdater = new DexterityVestQualityUpdater();
+            _sellInUpdater = new StandardSellInUpdater();
+        }
+
+        public static string ItemNameToProcess() => "+5 Dexterity Vest";
+
+        public Item UpdateItem(Item item)
+        {
+            _dexterityVestQualityUpdater.UpdateQuality(item);
+            _sellInUpdater.UpdateSellIn(item);
+
+            return item;
+        }
+
+    }
+}
